a3f8c474183e2a3f397a531fab7796857a795d9f
scarlos723 <carlossde58@gmail.com>
2022-02-02 03:56:41 -0500

    pages implemented and funtional

---
+++ lib/pages/beacons_page.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_beacon/flutter_beacon.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'dart:async';
+
+class BeaconsPage extends StatefulWidget {
+  const BeaconsPage({Key? key}) : super(key: key);
+
+  @override
+  _BeaconsPageState createState() => _BeaconsPageState();
+}
+
+class _BeaconsPageState extends State<BeaconsPage> {
+  var points = [
+    {
+      'uid': 'FDA50693-A4E2-4FB1-AFCF-C6EB07647825',
+      'message': 'gire a la derecha por favor',
+    },
+    {
+      'uid': 'D546DF97-4757-47EF-BE09-3E2DCBDD0C77',
+      'message': 'gire a la derecha por favor',
+    }
+  ];
+  Map<String, bool> way = {
+    'FDA50693-A4E2-4FB1-AFCF-C6EB07647825': false,
+    'D546DF97-4757-47EF-BE09-3E2DCBDD0C77': false
+  };
+
+  // StreamSubscription<RangingResult>? _streamRanging;
+  // StreamSubscription<BluetoothState>? _streamBluetooth;
+  // final _regionBeacons = <Region, List<Beacon>>{};
+  // final _beacons = <Beacon>[];
+
+  //State TTS
+  FlutterTts flutterTts = FlutterTts();
+
+  Future _speakMessage(description) async {
+    await flutterTts.setVolume(1);
+    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.setPitch(1.0);
+    await flutterTts.setLanguage('es-ES');
+    await flutterTts.awaitSpeakCompletion(true);
+    await flutterTts.speak(description);
+  }
+
+  void _handlerLists(beacons) {
+    try {
+      for (var item in points) {
+        if (item['uid'] == beacons[0].proximityUUID) {
+          print(way[item['uid']]);
+          if (way[item['uid']] == false) {
+            _speakMessage(item['message']);
+            setState(() {
+              way[item['uid'].toString()] = true;
+            });
+          }
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _beaconRead() {
+    //beacons con los que trabajaremos
+    final regions = <Region>[
+      Region(
+        identifier: 'iBeacon2.0',
+        proximityUUID: 'D546DF97-4757-47EF-BE09-3E2DCBDD0C77', //+encontrado
+      ),
+      Region(
+        identifier: 'iBeacon2.1',
+        proximityUUID: 'FDA50693-A4E2-4FB1-AFCF-C6EB07647825',
+      ),
+    ];
+    void _initBeacon() async {
+      try {
+        // if you want to manage manual checking about the required permissions
+        await flutterBeacon.initializeScanning;
+        // or if you want to include automatic checking permission
+        //await flutterBeacon.initializeAndCheckScanning;
+      } on FormatException catch (e) {
+        // library failed to initialize, check code and message
+        print(e);
+      }
+    }
+
+    void _requestBeacon() async {
+      await flutterBeacon
+          .requestAuthorization; // es necesario pedir permisos al usuario para poder tener una respuesta!!
+    }
+
+    _requestBeacon();
+    _initBeacon();
+
+    flutterBeacon.ranging(regions).listen((RangingResult result) {
+      //print(result.beacons);
+      _handlerLists(result.beacons);
+      // for (var item in points) {
+      //   if (item['uid'] == result.beacons[0].proximityUUID) {
+      //     print(item['message']);
+      //   }
+      // }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _beaconRead();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the SpeechPage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: const Text('Beacons'),
+        ),
+        body: const Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Text("Sigue el camino")));
+  }
+}
---
+++ lib/pages/speech_page.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+import 'package:beacon_test/pages/beacons_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+class SpeechPage extends StatefulWidget {
+  const SpeechPage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<SpeechPage> createState() => _SpeechPageState();
+}
+
+class _SpeechPageState extends State<SpeechPage> {
+  // Speach States
+  final SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String _lastWords = '';
+  // TTS
+  FlutterTts flutterTts = FlutterTts();
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the SpeechPage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Recognized words:',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  // If listening is active show the recognized words
+                  _speechToText.isListening
+                      ? _lastWords
+                      // If listening isn't active but could be tell the user
+                      // how to start it, otherwise indicate that speech
+                      // recognition is not yet ready or not supported on
+                      // the target device
+                      : _speechEnabled
+                          ? 'Tap the microphone to start listening...'
+                          : 'Speech not available',
+                ),
+              ),
+            ),
+            GestureDetector(
+                onDoubleTap: () {
+                  _showBeaconPage(context);
+                },
+                onLongPress: _startListening,
+                onLongPressUp: _stopListening,
+                child: Container(
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    width: 300,
+                    height: 300,
+                    color: Colors.red))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:
+            // If not yet listening for speech start, otherwise stop
+            _speechToText.isNotListening ? _startListening : _stopListening,
+        tooltip: 'Listen',
+        child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+      ),
+    );
+  }
+
+  Future _speakMessage(description) async {
+    await flutterTts.setVolume(1);
+    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.setPitch(1.0);
+    await flutterTts.setLanguage('es-ES');
+    await flutterTts.awaitSpeakCompletion(true);
+    await flutterTts.speak(description);
+  }
+
+  // Functions to Speech library
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Each time to start a speech recognition session
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult, localeId: 'es_ES');
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    _speakMessage(result.recognizedWords);
+    setState(() {
+      _lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState(); // inicializa los estados
+    //Funcion periodica que resetea los puntos por si el usuario se queda en un solo punto
+    Timer.periodic(const Duration(seconds: 30), (timer) {
+      print("Reset CHEKK!!");
+    });
+    _initSpeech();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _showBeaconPage(BuildContext context) {
+    Navigator.pushNamed(context, 'beacons');
+  }
+}
